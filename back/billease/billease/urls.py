bfa55f38d7d6e7f8e4d04d2800b6a430b86884f6
Léa Cassegrain <lea.csgn@gmail.com>
2024-06-08 16:20:28 +0200

    Add client app with models, serializers, views

--- back/billease/billease/urls.py
+++ back/billease/billease/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
+    path('api/v1/', include('apps.client.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
